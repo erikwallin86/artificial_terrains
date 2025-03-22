02cb60173355c4a0dc242110f0790cfc640eed72
Erik Wallin <erikwallin86@gmail.com>
2025-03-22 18:14:16 +0100

    small fix in call numbers

--- modules/loop.py
+++ modules/loop.py
@@ -11,11 +11,11 @@ class Loop(Module):
         self.loop_generator_instance = self.loop_generator()
 
     def loop_generator(self):
+        call_total = self.n_loops
         for call_number in range(self.n_loops):
-            call_total = self.n_loops
             self.logger.info(f"Run {self.name} {call_number+1}/{call_total}")
             return_dict = {
                 'call_number': call_number,
-                'call_total': self.n_loops,
+                'call_total': call_total,
             }
             yield return_dict
