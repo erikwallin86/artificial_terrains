1ea9cd59b819dffbdec0d7617dfab30ee9186db6
Erik Wallin <erikwallin86@gmail.com>
2025-03-24 08:14:28 +0100

    Loop: update prints, comments, and call_total fix

--- modules/loop.py
+++ modules/loop.py
@@ -6,23 +6,27 @@ class Loop(Module):
     ''' Basic loop '''
     create_folder = False
 
-    def start(self, default=None, n_loops=2, loop_id=None, **kwargs):
+    def start(self, default=None, n_loops=2, loop_id=None, call_total=None, **kwargs):
         self.n_loops = default if default is not None else n_loops
         self.loop_generator_instance = self.loop_generator()
 
         self.loop_id = "" if loop_id is None else loop_id
         self.digits = int(2 + np.log10(self.n_loops))
 
-    def loop_generator(self):
-        call_total = self.n_loops
-        for call_number in range(self.n_loops):
-            self.logger.info(f"Run {self.name} {call_number+1}/{call_total}")
+        self.call_total = call_total * self.n_loops
 
+    def loop_generator(self):
+        for call_number in range(self.n_loops):
+            # Custom print, as the debug_decorator prints don't work for the
+            # generator setup. Also, here we loop over 'self.n_loops'
+            self.logger.info(f"Run {self.name} {call_number+1}/{self.n_loops}")
+
+            # Update loop-id, to be used in filenames
             loop_id = self.loop_id + f"_{call_number:0{self.digits}d}"
 
             return_dict = {
                 'call_number': call_number,
-                'call_total': call_total,
+                'call_total': self.call_total,
                 'loop_id': loop_id,
             }
             yield return_dict
