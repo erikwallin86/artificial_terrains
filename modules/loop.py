55a4d42e0177a9d574ee0bf31d9c56689a302d7e
Erik Wallin <erikwallin86@gmail.com>
2025-03-24 08:38:12 +0100

    get call number and total running, with multiple loops

--- modules/loop.py
+++ modules/loop.py
@@ -6,14 +6,21 @@ class Loop(Module):
     ''' Basic loop '''
     create_folder = False
 
-    def start(self, default=None, n_loops=2, loop_id=None, call_total=None, **kwargs):
+    def start(self, default=None, n_loops=2, loop_id=None, call_total=None,
+              call_number=None, **kwargs):
         self.n_loops = default if default is not None else n_loops
         self.loop_generator_instance = self.loop_generator()
 
         self.loop_id = "" if loop_id is None else loop_id
         self.digits = int(2 + np.log10(self.n_loops))
 
-        self.call_total = call_total * self.n_loops
+        # We save the raw input
+        self.call_total = call_total
+        self.call_number = call_number
+
+        self.logger.info(f"INIT {self.name}")
+        print(f"##### self.call_total:{self.call_total}")
+        print(f"##### self.call_number:{self.call_number}")
 
     def loop_generator(self):
         for call_number in range(self.n_loops):
@@ -24,9 +31,13 @@ class Loop(Module):
             # Update loop-id, to be used in filenames
             loop_id = self.loop_id + f"_{call_number:0{self.digits}d}"
 
+            # Construct return_dict, and fix the call_total and call_number
+            # in case of several loops.
+            # call_total: multiply with length of the number of loops in this
+            # call_number: add number of previous
             return_dict = {
-                'call_number': call_number,
-                'call_total': self.call_total,
+                'call_total': self.call_total * self.n_loops,
+                'call_number': call_number + self.call_number*self.n_loops,
                 'loop_id': loop_id,
             }
             yield return_dict
