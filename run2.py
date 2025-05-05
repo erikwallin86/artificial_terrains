064f437ebca6672dec213e61de92501acd413fd1
Erik Wallin <erikwallin86@gmail.com>
2025-05-05 14:22:36 +0200

    run2.py: update to also run via blender --python ... --

--- run2.py
+++ run2.py
@@ -1,4 +1,18 @@
 import os
+import sys
+try:
+    import bpy
+    # Make sure current dir is in path. A blender py workaround
+    dir = os.path.dirname(bpy.data.filepath)
+    if dir not in sys.path:
+        sys.path.append(dir)
+except Exception:
+    pass
+else:
+    from modules.blender import fix_blender_argv, fix_blender_path
+    fix_blender_path()
+    fix_blender_argv()
+
 from utils.parse_utils import create_parser, get_args_combined_with_settings
 from modules.modules import MODULES
 from utils.logging_utils import get_logger, level_map
